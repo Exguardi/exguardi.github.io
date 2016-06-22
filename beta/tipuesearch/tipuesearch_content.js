e07c443479276c9880daa17c1d820679bdfcb99e
Robert Speck <bobby.s.speck@gmail.com>
2016-06-22 16:22:33 -0700

    - Updated "featuredWork" links
    Former-commit-id: 3c9e9362f2239ef79d4588bdd0e4ca26bc8965c7 [formerly e889294600af798ac8ef162b723b508e98c4a567] [formerly 81dd0d5c3c0d3c82e87653f4d0be8550496149eb [formerly 022e1ca26758d3b1032240be4199ddfa49e418dc]]
    Former-commit-id: fecd11f8b8bd2affc1cc1d44f405671682f8434a [formerly d1dc71bd3a274ffdee1c1369a9d8b7a2e3e23e7e]
    Former-commit-id: bbe0716d4b06b40bb930db25a1936cd0b221efbb

--- beta/tipuesearch/tipuesearch_content.js
+++ beta/tipuesearch/tipuesearch_content.js
@@ -2,5 +2,5 @@
 var tipuesearch = {"pages": [
      /*{"title": "Tipue", "text": "", "tags": "jQuery HTML5 CSS", "url": "http://www.tipue.com"},*/
      {"title": "App Dev & Technology | speck.tech", "text": "", "tags": "speck.tech home featured", "url": "/"},
-     {"title": "Featured Work | speck.tech", "text": "", "tags": "featured portfolio", "url": "/#featuredWork"}
+     {"title": "Featured Work | speck.tech", "text": "", "tags": "featured portfolio", "url": "/work.html"}
 ]};
